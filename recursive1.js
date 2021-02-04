8708943e6d41162132c6ea923c1e90da46ddc6de
FRANSISCUS ASISI UTOMO HADI <67340848+utomo1986@users.noreply.github.com>
2021-02-05 02:05:33 +0700

    Add files via upload

---
+++ recursive1.js
@@ -0,0 +1,20 @@
+/*
+1. Buatlah rekursive function void (tidak mengembalikan nilai) yang dapat menampilkan deret aritmatika genap sampai N input. 
+
+Contoh:
+f(5) -> 0 2 4
+f(8) -> 0 2 4 6 8
+*/
+
+function aritmatika(n) {
+    if (n == 0) {
+        console.log(n);
+    } else {
+        aritmatika(n - 1);
+        if (n % 2) {
+            console.log(n);
+        }
+    }
+}
+console.log(aritmatika(5))
+console.log(aritmatika(8))
